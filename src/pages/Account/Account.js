de2d157b334e4cf7ba86e267b5b65ee0c54d83d5
Macbook <tin.caotrong@gmail.com>
2020-12-11 21:59:07 +0700

    create-course-teacher

---
+++ src/pages/Account/Account.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Row, Col, Divider, Avatar, Layout, Button } from 'antd';
+import Career from '../../static/images/Carreer.png';
+import InfoForm from './components/InfoForm';
+import './style.css';
+
+import HotCourses from '../../components/HotCourses';
+
+
+class Account extends React.Component {
+    state = {
+       
+    };
+
+    render() {
+        
+        return (
+            <div className='account'>
+                <Row style={{ paddingTop: '20px', paddingBottom: '0px', paddingLeft: '200px', paddingRight: '200px' }}>
+                    <div className="card-achieve" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        <Avatar size={160} src={Career}></Avatar>
+                        <Button>Upload</Button>
+                        <span className="goal" style={{ fontSize: '50px' }}>Ben Dover</span>
+                        <Divider></Divider>
+                    </div>
+                </Row>
+                <Row style={{ paddingTop: '0px', paddingBottom: '0px', paddingLeft: '200px', paddingRight: '200px' }}>
+                    <div className="card-achieve" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        <InfoForm></InfoForm>
+                        <Divider></Divider>
+                    </div>
+                </Row>
+                <Row type="flex" justify="center">
+                    <Col span={22} style={{ margin: '0 auto'}}>
+                    <h2 style={{ fontSize: '32px', color: '#3c3b37', fontWeight:'800'}}>My Watch List</h2>
+                    <Row style={{ paddingBottom: '100px'}}>
+                    <HotCourses/>
+                    </Row>
+                    </Col>
+                </Row>
+                <Row type="flex" justify="center">
+                    <Col span={22} style={{ margin: '0 auto'}}>
+                    <h2 style={{ fontSize: '32px', color: '#3c3b37', fontWeight:'800'}}>Courses I have taken</h2>
+                    <Row style={{ paddingBottom: '100px'}}>
+                    <HotCourses/>
+                    </Row>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default Account;
